bb32772f2a1dab6f3d6d6e8cb29210def7fd112f
Edvard Strand Edding <edvard.edding@unil.ch>
2019-11-12 11:02:41 +0100

    Add measure run time of estimate_pi to server.R

--- inst/shiny-examples/pi/server.R
+++ inst/shiny-examples/pi/server.R
@@ -1,26 +1,30 @@
 library(shiny)
 library(ptds2019hw4g06)
+library(tidyverse)
 
 shinyServer(function(input, output) {
 
     simulate <- reactive({
         # simulate pi and measure the time here
-        ...
+        start_time <- Sys.time()
+        ptds2019hw4g06::estimate_pi(input$B, input$seed)
+        end_time <- Sys.time()
     })
 
     output$plot <- renderPlot({
         # plot pi
-        ...
+        ptds2019hw4g06:::plot.pi()
     })
 
     output$time <- renderText({
         # extract the time of the execution
-        ...
+        time <- end_time - start_time
+        print(time)
     })
 
     output$pi <- renderText({
         # extract the estimated value
-        ...
+
     })
 
 })
