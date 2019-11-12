780b38a216d9d4478513e917c4b9d081e8874b6f
NielsSaalfeldt <niels.saalfeldt@unil.ch>
2019-11-12 11:02:31 +0100

    Working on server

--- inst/shiny-examples/pi/server.R
+++ inst/shiny-examples/pi/server.R
@@ -5,22 +5,21 @@ shinyServer(function(input, output) {
 
     simulate <- reactive({
         # simulate pi and measure the time here
-        ...
+
     })
 
     output$plot <- renderPlot({
         # plot pi
-        ...
+
     })
 
     output$time <- renderText({
         # extract the time of the execution
-        ...
+
     })
 
     output$pi <- renderText({
-        # extract the estimated value
-        ...
+
     })
 
 })
--- inst/shiny-examples/pi/ui.R
+++ inst/shiny-examples/pi/ui.R
@@ -8,11 +8,11 @@ shinyUI(fluidPage(
 
         sidebarPanel(
 
-            selectInput("method", label = "estimate pi", choices = c("estimate_pi()","find_pi") ),
 
-            numericInput("seed", label = "Choose a numerical value", value = 10),
 
-            sliderInput("B", label = "Choose a number between 1 and 1000000", min = 1, max = 1000000, value = 5000)
+            numericInput("seed", label = "Choose a value for seed", value = 10),
+
+            sliderInput("B", label = "Choose a number below", min = 1, max = 1000000, value = 5000)
 
         ),
 
