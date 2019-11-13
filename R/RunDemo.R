188539a9f3ef380a4b0ba221f8fc3bbb7d562fcd
Ferdi28 <49550626+Ferdi28@users.noreply.github.com>
2019-11-13 21:18:23 +0100

    Update RunDemo.R

--- R/RunDemo.R
+++ R/RunDemo.R
@@ -7,11 +7,9 @@
 #'
 #' @export
 runDemo <- function() {
-  # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
   appDir <- system.file("shiny-examples", "pi", package = "ptds2019hw4g06")
   if (appDir == "R/RunDemo.R") {
     stop(
-      # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
       "Could not find example directory. Try re-installing ptds2019hw4g06.",
       call. = FALSE
     )
