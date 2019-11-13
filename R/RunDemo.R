e9d089b76e091aa41a383981c6b116a74d3352d1
NielsSaalfeldt <niels.saalfeldt@unil.ch>
2019-11-13 15:23:23 +0100

    Update RunDemo.R

--- R/RunDemo.R
+++ R/RunDemo.R
@@ -1,5 +1,5 @@
 #' @title Shiny Application of Montecarlo estimation of pi
-#' @description
+#' @description Run a shiny app for Montecarlo estimation of pi
 #' @import shiny
 #' @import ptds2019hw4g06
 #' @import tidyverse
@@ -9,7 +9,7 @@
 runDemo <- function() {
   # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
   appDir <- system.file("shiny-examples", "pi", package = "ptds2019hw4g06")
-  if (appDir == "") {
+  if (appDir == "R/RunDemo.R") {
     stop(
       # REPLACE N BY YOUR GROUP NUMBER AND DELETE THIS COMMENT
       "Could not find example directory. Try re-installing ptds2019hw4g06.",
