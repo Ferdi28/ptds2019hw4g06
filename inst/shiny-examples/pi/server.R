10586581ee1421f9a773b18dd0cb16a764cf4368
Ferdi28 <49550626+Ferdi28@users.noreply.github.com>
2019-11-14 15:40:21 +0100

    Update server.R
    updated the Pi and time prints. They should work as reactive functions now

--- inst/shiny-examples/pi/server.R
+++ inst/shiny-examples/pi/server.R
@@ -5,42 +5,28 @@ library(lubridate)
 
 shinyServer(function(input, output) {
 
-#rv <- reactiveValues(startTime = Sys.time(), endTime = Sys.time())
-        simulate <- reactive({
-        # simulate pi and measure the time here
-
-        #req(input$B || input$seed)
-        #rv$startTime <- Sys.time()
-        #pi <- estimate_pi(B = input$B, seed = input$seed)
-        #rv$endTime <- Sys.time()
-
-    ptds2019hw4g06::estimate_pi(input$B, input$seed)
-
-    system.time(ptds2019hw4g06::estimate_pi(input$B, input$seed))
-
+        #reactive function for Pi and CPU time
+        simulated_pi <- reactive({
+          estimate_pi(input$B, input$seed)[["estimated_pi"]]
     })
+        simulated_time <- reactive({
+          system.time(estimate_pi(input$B, input$seed))["elapsed"]*1000
+        })
 
     output$plot <- renderPlot({
-        # plot pi
-
-# plot.pi(estimate_pi(input$B, input$seed))
-
-    ptds2019hw4g06:::plot.pi(estimate_pi(input$B, input$seed))
-    },width = 400, height = 400)
+      plot(estimate_pi(input$B, input$seed))
+    },
+    width = 400, height = 400)
 
     output$time <- renderText({
         # extract the time of the execution
-    #paste("Elapsed time: ",rv$endTime - rv$startTime)
-        paste("Elapsed time: ", system.time(ptds2019hw4g06::estimate_pi(input$B, input$seed))["elapsed"])
+        paste("Elapsed time to estimate Pi in Milliseconds: ", simulated_time(),"If it is 0, the amount of time taken was negligible", sep ="\n")
     })
+
     output$pi <- renderText({
         # extract the estimated value
-
-        pi <- estimate_pi(B = input$B,seed = input$seed )
-        paste("Value of Estimated Pi :",
-              pi[["estimated_pi"]])
+        paste0("Value of Estimated Pi : ",simulated_pi())
     })
-
 
 
 
